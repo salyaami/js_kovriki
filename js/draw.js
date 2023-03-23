ad93028f8ac1094fc934d1952a8211cf1bf67b2f
salyaami <74817710+salyaami@users.noreply.github.com>
2023-03-23 21:07:31 +0300

    Add files via upload

--- js/draw.js
+++ js/draw.js
@@ -1,18 +1,76 @@
+let prevX, prevY, x1, y1;
+
 $(document).ready(function() {
 
   let canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d");
 
-  let drawing = (e) => {
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.stroke();
+  let windowWidth = window.innerWidth
+
+  if (windowWidth > 800) {
+    canvas.width = 800
+    canvas.height = 800
   }
-  ctx.strokeStyle = "blue";
+
+  // let drawing = (e) => {
+  //   console.log(e);
+  //   ctx.lineTo(e.offsetX, e.offsetY);
+  //   // const canvasPosition = canvas.getBoundingClientRect()
+  //   // const canvasTop = canvasPosition.top
+  //   // const canvasLeft = canvasPosition.left
+  //
+  //   // ctx.lineTo(e.clientX - canvasLeft, e.clientY - canvasTop);
+  //   ctx.stroke();
+  // }
+    ctx.strokeStyle = "blue";
   // $('#black_knopka').click(function() {
   //   ctx.strokeStyle = "black";
   // });
     ctx.lineWidth = 3;
   canvas.addEventListener("mousemove", drawing);
+
+  function draw(e) {
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    console.log(e);
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 4;
+    if(e.buttons == 1) {
+      drawLine(prevX, prevY, x, y);
+      drawLine(800 - prevX, 800 - prevY, 800 - x, 800 - y);
+      drawLine(800 - prevX, prevY, 800 - x, y);
+      drawLine(prevX, 800 - prevY, x, 800 - y);
+      drawLine(prevY, prevX, y, x);
+      drawLine(800 - prevY, 800 - prevX, 800 - y, 800 - x);
+      drawLine(800 - prevY, prevX, 800 - y, x);
+      drawLine(prevY, 800 - prevX, y, 800 - x);
+    }
+    prevX = x;
+    prevY = y;
+}
+
+
+  function drawing(x1, y1, x2, y2) {
+   ctx.beginPath();
+   ctx.moveTo(x1, y1);
+   ctx.lineTo(x2, y2);
+   ctx.stroke();
+  }
+
+
+
+
+  $(window).resize(() => {
+    console.log('resize');
+
+    windowWidth = window.innerWidth
+
+    if (windowWidth > 800) {
+      canvas.width = 300
+      canvas.height = 300
+    }
+  })
 });
 
 //
--- js/track.js
+++ js/track.js
@@ -1,23 +1,40 @@
-$(document).ready(function(){
-  let arr = ['img/apple.png', 'img/lemon.png', 'img/list.png', 'img/black.png',
-  'img/brocco.png', 'img/carrot.png', 'img/fish.png',
+let rand, canvasPosition, canvasTop, canvasLeft;
+
+let arr = ['img/apple.png', 'img/lemon.png', 'img/list.png', 'img/black.png',
+'img/brocco.png', 'img/carrot.png', 'img/fish.png',
 'img/mush.png', 'img/straw.png'];
 
 let arr_small = ['img/apple2.png', 'img/lemon2.png', 'img/list2.png', 'img/black2.png',
 'img/brocco2.png', 'img/carrot2.png', 'img/fish2.png',
 'img/mush2.png', 'img/straw2.png'];
 
-  let rand;
-  $(document).mousemove(function(e) {
+$(document).ready(function(){
+
+  $('.rectangle_3').mousemove(function(e) {
+    console.log($(window).width(), $(window).width() <= 767);
     if ($(window).width() <= 767) {
       rand = Math.floor(Math.random() * arr_small.length);
+      element = arr_small[rand]
+      console.log('arr1');
     } else {
       rand = Math.floor(Math.random() * arr.length);
+      element = arr[rand]
+      console.log('arr2');
     }
 
-   let img = $("<img>").attr("src", arr[rand]).css({
-    "left": e.pageX + "px",
-    "top": e.pageY + "px",
+    // console.log(e.offsetX, e.offsetY);
+
+    canvasPosition = this.getBoundingClientRect()
+    canvasTop = canvasPosition.top
+    canvasLeft = canvasPosition.left
+
+    console.log(e.clientX - canvasLeft, e.clientY - canvasTop);
+
+   let img = $("<img>").attr("src", element).css({
+    // "left": e.pageX + "px",
+    // "top": e.pageY + "px",
+    "left": e.clientX - canvasLeft + "px",
+    "top": e.clientY - canvasTop + "px",
     "position": "absolute"
   }).appendTo(".rectangle_3").fadeOut(7000);
   });
